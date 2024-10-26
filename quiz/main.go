0b7e705c92f2c8679e7d5198efc9b5726e80aa64
roshan <amalazimroshan@gmail.com>
2024-10-26 14:41:04 +0530

    completed quiz part two implemented timer functionality

--- quiz/main.go
+++ quiz/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
 	"strconv"
+	"time"
 )
 
 type Question struct {
@@ -16,8 +18,9 @@ type Question struct {
 
 func main() {
 
-	args := os.Args[1:]
-	var numberOfQuestions int
+	numberOfQuestions := flag.Int("questions", -1, "number of questions")
+	timeLimit := flag.Int("limit", 30, "time limit for one question")
+	flag.Parse()
 
 	file, err := os.Open("problems.csv")
 	if err != nil {
@@ -44,29 +47,43 @@ func main() {
 		questions[i], questions[j] = questions[j], questions[i]
 	})
 
-	if len(args) > 0 {
-		num, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println("invalid argument Switching to default state")
-			numberOfQuestions = len(questions)
-		} else {
-			numberOfQuestions = num
-		}
-	} else {
-		numberOfQuestions = len(questions)
+	if *numberOfQuestions < 0 {
+		*numberOfQuestions = len(questions)
 	}
+
+	fmt.Println("Press enter to start the quizz")
+	fmt.Scanln()
 
 	score := 0
-	for i := 0; i < numberOfQuestions; i++ {
-		fmt.Printf("Solve: %s =", questions[i].problem)
-		var userAnswer int
-		fmt.Scanln(&userAnswer)
-		if userAnswer == questions[i].answer {
-			score++
-		} else {
-			fmt.Println("incorrect! the correct answer was:", questions[i].answer)
-		}
-	}
+	timeDuration := time.Duration(*timeLimit**numberOfQuestions) * time.Second
+	timer := time.NewTimer(timeDuration)
+	done := make(chan bool)
 
-	fmt.Printf("you scored %d out of %d", score, numberOfQuestions)
+	fmt.Printf("you have a total of %v to complete the quiz\n", timeDuration)
+	go func() {
+		for i := 0; i < *numberOfQuestions; i++ {
+			select {
+			case <-done:
+				return
+			default:
+				fmt.Printf("Solve: %s =", questions[i].problem)
+				var userAnswer int
+				fmt.Scanln(&userAnswer)
+				if userAnswer == questions[i].answer {
+					score++
+				} else {
+					fmt.Println("incorrect! the correct answer was:", questions[i].answer)
+				}
+			}
+		}
+		done <- true
+	}()
+
+	select {
+	case <-timer.C:
+		fmt.Println("\n times up!")
+	case <-done:
+		fmt.Println("\nquiz completed")
+	}
+	fmt.Printf("you scored %d out of %d", score, *numberOfQuestions)
 }
